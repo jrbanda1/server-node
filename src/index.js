485d531dd8082e1dc868ca56eb0ded4288148c4a
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-10-22 09:13:11 -0500

    removed mathStuff.js

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import { multiplication } from "./mathStuff";
-
-console.log(multiplication(3,5))
--- src/mathStuff.js
+++
@@ -1,3 +0,0 @@
-export const sum = (x, y) => x + y
-
-export const multiplication = (x, y) => x * y
