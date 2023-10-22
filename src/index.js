b0600ce6e4830415d98d70a3ccfc02c69e070ce2
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-10-22 11:37:00 -0500

    added helmet, cors, morgan

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
+import './helpers/doeenv'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helment from 'helmet'
+
+const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
 
-const port = 3000
+app.use(morgan(process.env.MORGAN_LOG))
+app.use(cors({ origin: process.env.ORIGIN }))
+app.use(helment())
 
-app.get('/', (req,res) =>{
-    res.send({msg: 'Hello There'})
+app.get('/', (req, res) => {
+  const title = process.env.TITLE || 'Server'
+  res.send({ msg: title })
 })
 
 app.listen(port)
